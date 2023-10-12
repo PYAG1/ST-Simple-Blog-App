5cd1d6ea84acc6c3219c1e14451a06e9a960fbdc
PYAG1 <gyekyeyaw3@gmail.com>
2023-10-12 12:23:15 +0000

    React Quill Functionality

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <head>
+      <link
+  rel="stylesheet"
+  href="https://unpkg.com/react-quill@1.3.3/dist/quill.snow.css"
+/>
+      </head>
       <body>
         <Main />
         <NextScript />
--- src/pages/add_blog/index.tsx
+++ src/pages/add_blog/index.tsx
@@ -1,50 +1,118 @@
-
 import Link from "next/link";
 import React from "react";
 import { FaArrowLeft } from "react-icons/fa";
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { title } from "process";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import dynamic from "next/dynamic";
+import { useField } from "formik";
+
+const ReactQuill = dynamic(() => import("react-quill"), {
+  ssr: false, // Ensure it's not loaded on the server side
+});
+import "react-quill/dist/quill.snow.css";
 
 
-export default function index() {
-  const newDate = new Date();
+interface ReactQuillFieldProps {
+  label: string;
+  name: string;
+}
 
-  const day = String(newDate.getUTCDate()).padStart(2, '0');
-  const month = String(newDate.getUTCMonth() + 1).padStart(2, '0'); // Month is zero-based, so we add 1.
-  const year = newDate.getUTCFullYear();
-  
-  const formattedDate = `${day}/${month}/${year}`;
-  
-  console.log(formattedDate);
-  
+const ReactQuillField:React.FC<ReactQuillFieldProps> = ({ label, ...props }) => {
+  const [field, , helpers] = useField(props);
+  const { value } = field;
 
-
+  const modules = {
+    toolbar: [
+      ["bold", "italic", "underline", "strike"],
+      ["link"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["clean"],
+    ],
+  };
 
   return (
     <div>
-      <header className=" flex justify-between items-center p-3">
+      <label htmlFor={props.name} className="text-3xl">
+        {label}
+      </label>
+      <ReactQuill
+        value={value || ''}
+        onChange={(content) => helpers.setValue(content)}
+        modules={modules}
+      />
+    </div>
+  );
+};
+
+export default function index() {
+  const newDate = new Date();
+  const day = String(newDate.getUTCDate()).padStart(2, "0");
+  const month = String(newDate.getUTCMonth() + 1).padStart(2, "0");
+  const year = newDate.getUTCFullYear();
+  const formattedDate = `${day}/${month}/${year}`;
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string()
+      .required("Title is required")
+      .min(1, "Title must not be an empty string"),
+    content: Yup.string()
+      .required("Content is required")
+      .min(1, "Content must not be an empty string"),
+  });
+
+  return (
+    <div>
+      <header className="flex justify-between items-center p-3">
         <Link href={"/"}>
           <FaArrowLeft size={30} />
         </Link>
       </header>
 
+      <Formik
+        initialValues={{
+          title: "",
+          content: "",
+          date: formattedDate,
+          author: "you",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values) => {
+          // Handle form submission logic here
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form className="w-full flex flex-col gap-4 px-[1.5em]">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="title" className="text-3xl">
+                Title
+              </label>
+              <Field
+                type="text"
+                id="title"
+                name="title"
+                placeholder="Enter your title"
+                className={`focus:border-none border-[2px] ${
+                  !errors.title
+                    ? "focus:outline-[#4FFFB0] outline-black/25"
+                    : "focus:outline-[#EF0107] border-[#EF0107]"
+                } focus:outline-[2px] px-4 py-2 rounded duration-700 text-black/80 bg-slate-50`}
+              />
+              {errors.title && (
+                <ErrorMessage
+                  name="title"
+                  component="div"
+                  className="text-red-600"
+                />
+              )}
+            </div>
 
+            <ReactQuillField name="content" label="Content" />
 
-      
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
-/*      <Formik 
-      initialValues={
-    {
-      title:"",
-      content:"",
-      date:``,
-      author:"you"
-    }
-      }
-      >
-        <Form>
-
-        </Form>
-      </Formik>*/
